d588741fc0417e1a250b020a234302fea3f43beb
fjramireg <fjramireg@gmail.com>
2020-02-03 16:07:42 -0500

    Updated 'Content' file.

--- Scalar/Contents.m
+++ Scalar/Contents.m
@@ -18,25 +18,35 @@
 %		(a): Only if computes ALL elements data
 
 %% Functions Files
+%
+% --- INDEX COMPUTATION ---
 %   Index_sss              - Computes the row/column indices of tril(K) for a SCALAR (s)
 %   Index_sps              - Computes the row/column indices of tril(K) for a SCALAR (s)
+%
+% --- ELEMENT STIFFNESS COMPUTATION ---
 %   eStiff_ss              - Computes the element stiffnes matrix for a SCALAR (s) problem
 %   eStiff_sss             - Computes the element stiffness matrix for a SCALAR (s) problem
 %   eStiff_sssa            - Computes the element stiffness matrices for a SCALAR (s)
 %   eStiff_spsa            - Computes the element stiffness matrices for a SCALAR (s)
-%   StiffMa_sps            - Create the global stiffness matrix tril(K) for a SCALAR (s)
+%
+% --- GLOBAL STIFFNESS COMPUTATION ---
 %   StiffMa_ss             - Create the global stiffness matrix K for a SCALAR (s) problem
 %   StiffMa_sss            - Create the global stiffness matrix tril(K) for a SCALAR (s)
+%   StiffMa_sps            - Create the global stiffness matrix tril(K) for a SCALAR (s)
 
 %% Script Files used to run the functions
-%   runScalarCPUvsGPU      - Runs the whole assembly scalar. CPU vs GPU
-%   runScalarOnCPU         - Runs the whole assembly scalar code on the CPU
-%   runScalarOnGPU         - Runs the whole assembly scalar code on the GPU
-%   runHex8ScalarCPUvsGPU  - Runs the HEX8 scalar code. CPU vs GPU
-%   runHex8ScalarOnCPU     - Runs the HEX8 scalar code on the CPU
-%   runHex8ScalarOnGPU     - Runs the HEX8 scalar code on the GPU
-%   runIndexScalarCPUvsGPU - Runs the INDEX scalar code. CPU vs GPU
+%
+% --- INDEX COMPUTATION ---
 %   runIndexScalarOnCPU    - Runs the INDEX scalar code on the CPU
 %   runIndexScalarOnGPU    - Runs the INDEX scalar code on the GPU
-
-
+%   runIndexScalarCPUvsGPU - Runs the INDEX scalar code. CPU vs GPU
+%
+% --- ELEMENT STIFFNESS COMPUTATION ---
+%   runHex8ScalarOnCPU     - Runs the HEX8 scalar code on the CPU
+%   runHex8ScalarOnGPU     - Runs the HEX8 scalar code on the GPU
+%   runHex8ScalarCPUvsGPU  - Runs the HEX8 scalar code. CPU vs GPU
+%
+% --- GLOBAL STIFFNESS COMPUTATION ---
+%   runScalarOnCPU         - Runs the whole assembly scalar code on the CPU
+%   runScalarOnGPU         - Runs the whole assembly scalar code on the GPU
+%   runScalarCPUvsGPU      - Runs the whole assembly scalar. CPU vs GPU
