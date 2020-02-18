e6d74292b6ebb5c23ed56fde921bd96499f3018f
fjramireg <fjramireg@gmail.com>
2020-02-18 18:44:12 -0500

    Routine 'Index_va.m' in its final version!

--- Vector/Index_va.m
+++ Vector/Index_va.m
@@ -16,30 +16,17 @@ function [iK, jK] = Index_va(elements, sets)
 
 %   Written by Francisco Javier Ramirez-Gil, fjramireg@gmail.com
 %   Universidad Nacional de Colombia - Medellin
-%   Created:  18/02/2020. Version: 1.0
+%   Created:  18/02/2020. Version: 1.3
 
 if ( size(elements,1)~=8 )	% Check if the array "elements" is 8xnel
     error('Input "elements" must be a 8xnel array');
 end
 
-% ind = [3*elements(:)-2, 3*elements(:)-1, 3*elements(:)];
-% ind_i2 = ind';
-% ind_i3 = ind_i2(:);
-% ind_i3 = reshape(ind', sets.nxe*sets.nel*sets.dxn,  1);
-% ind_i3 = reshape([3*elements(:)-2, 3*elements(:)-1, 3*elements(:)]', sets.edof*sets.nel,  1);
-ind = reshape([3*elements(:)-2, 3*elements(:)-1, 3*elements(:)]', sets.edof*sets.nel,  1);
-% ind_i4 = reshape(ind_i3, sets.edof, sets.nel);
-% ind_i4 = reshape([3*elements(:)-2, 3*elements(:)-1, 3*elements(:)]',  sets.edof, sets.nel);
-% ind_i5 = repmat(reshape(ind_i3, sets.edof, sets.nel), sets.edof, 1);
-% iK = ind_i5(:);
-iK = reshape(repmat(reshape(ind, sets.edof, sets.nel), sets.edof, 1), sets.edof^2*sets.nel, 1);
-
-% ind_j = repmat(ind, 1, sets.edof);
-% % ind_j = repmat(ind_i4, 1, sets.edof);
-% ind_j2 = ind_j';
-% jK = ind_j2(:);
-
-jK = reshape(repmat(ind, 1, sets.edof)', sets.edof^2*sets.nel, 1);
-
-% iK = reshape(repmat(elements2,sets.edof,1), sets.edof^2*sets.nel, 1);        % Computes & stores the row indices
-% jK = reshape(repmat(elements2(:),1,sets.edof)', sets.edof^2*sets.nel, 1);    % Computes & stores the column indices
+dofs = reshape([3*elements(:)-2, 3*elements(:)-1, 3*elements(:)]', ...
+                sets.edof*sets.nel,  1);        % 3 DOFs per node
+            
+iK = reshape(repmat(reshape(dofs, sets.edof, sets.nel), sets.edof, 1), ...
+                sets.edof^2*sets.nel, 1);       % Computes & stores the row indices
+            
+jK = reshape(repmat(dofs, 1, sets.edof)', ...
+                sets.edof^2*sets.nel, 1);       % Computes & stores the column indices
