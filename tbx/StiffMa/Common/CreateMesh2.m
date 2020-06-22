1d4e42fbdf126c806fdcba8b63931abec0cde045
Francisco <fjramireg@gmail.com>
2020-06-22 06:07:19 -0500

    CreateMesh2 function was modified with less inputs and improved documentation.

--- tbx/StiffMa/Common/CreateMesh2.m
+++ tbx/StiffMa/Common/CreateMesh2.m
@@ -4,8 +4,8 @@ function [elements, nodes] = CreateMesh2(nelx,nely,nelz,sets)
 % 
 %  INPUT:
 %   nelx, nely, nelz:   Number of elements on X-Y-Z direction
-%   dTN:                Data precision for "nodes" ['single' or 'double']
-%   dTE:                Data precision for "elements" ['uint32','uint64']
+%   sets.dTN:           Data precision for "nodes" ['single' or 'double']
+%   sets.dTE:          	Data precision for "elements" ['uint32','uint64']
 %
 %  OUTPUT:
 %   elements:           Conectivty matrix of the elements [nelx8]
@@ -18,26 +18,26 @@ function [elements, nodes] = CreateMesh2(nelx,nely,nelz,sets)
 
 %   Written by Francisco Javier Ramirez-Gil, fjramireg@gmail.com
 %   Universidad Nacional de Colombia - Medellin
-%   Created:  08 February 2020. Version: 1.0
-%   Modified: June 15th, 2020. Version: 1.1. Less inputs: sets instead of
-%   dTN and dTE
+%   Created:  February 08, 2020.    Version: 1.0
+%   Modified: June 22, 2020.        Versión: 1.1
+% 
 
 %% INPUTS CHECK
-if ~( mod(nelx,1)==0 && mod(nely,1)==0 && mod(nelz,1)==0 )   % Check if inputs "nel" are integers
+if ~( mod(nelx,1)==0 && mod(nely,1)==0 && mod(nelz,1)==0 )          % Check if inputs "nelX" are integers
     error('Error. Inputs "nelx", "nely" and "nely" must be integers');
-elseif ( nelx<0 || nely<0 || nelz<0 )                        % Check if "nel" are positives
+elseif ( nelx<0 || nely<0 || nelz<0 )                               % Check if "nel" are positives
     error('Error. Inputs "nelx", "nely" and "nely" must be positives');
-elseif ~( strcmp(sets.dTN,'single') || strcmp(sets.dTN,'double') )
-    error('Error. Input "dTN" must be "single" or "double"');
-elseif ~( strcmp(sets.dTE,'uint32') || strcmp(sets.dTE,'uint64') )
-    error('Error. Input "dTE" must be "uint32", "uint64"');
+elseif ~( strcmp(sets.dTN,'single') || strcmp(sets.dTN,'double') )  % Check data presicion for nodal coord.
+    error('Error. Input "sets.dTN" must be "single" or "double"');
+elseif ~( strcmp(sets.dTE,'uint32') || strcmp(sets.dTE,'uint64') )  % Check data presicion for connect. array
+    error('Error. Input "sets.dTE" must be "uint32", "uint64"');
 end
 
 %% NODES
-sX = ones(1,1,sets.dTN); sY = sX; sZ = sX; % Size of the paralelepiped on X-Y-Z direction
-selX = sX/nelx; selY = sY/nely; selZ = sZ/nelz; % Element size
-[X,Y,Z]= ndgrid(selX*(0:nelx),selY*(0:nely),selZ*(0:nelz));
-nodes = [X(:), Y(:), Z(:)];
+sX = ones(1,1,sets.dTN); sY = sX; sZ = sX;                  % Size of the paralelepiped on X-Y-Z direction
+selX = sX/nelx; selY = sY/nely; selZ = sZ/nelz;             % Element size
+[X,Y,Z]= ndgrid(selX*(0:nelx),selY*(0:nely),selZ*(0:nelz)); % Coordinates
+nodes = [X(:), Y(:), Z(:)];                                 % Nodal coordinates
 
 %% ELEMENTS
 if strcmp(sets.dTE,'uint32')
@@ -49,10 +49,10 @@ else
     ny = uint64(nely+1);
     nz = uint64(nelz+1);
 end
-nnod = nx*ny*nz;
-n = 1:nnod;                     % Selects all nodes at base of all elements
+nnod = nx*ny*nz;                % Number of nodes
+n = 1:nnod;                     % Nodes numbering
 fx = nx:nx:nnod;                % Nodes at face X=Sx
-fy = zeros(1,nz*nx,sets.dTE);        % Nodes at face Y=Sy
+fy = zeros(1,nz*nx,sets.dTE);   % Nodes at face Y=Sy
 nfy = nx*ny-ny:nx*ny:nnod;      % Node at edge X=0, Y=Sy
 for i=1:nz
     fy((i-1)*nx+1:(i-1)*nx+nx) = (1:nx)+(nfy(i)-1);
@@ -61,4 +61,6 @@ fz = nnod-nx*ny+1:nnod;         % Nodes at face Z=1
 n1 = setdiff(n',[fx,fy,fz]');   % Node 1 of each element
 elements = [n1, n1+1, n1+nx+1, n1+nx, n1+(nx*ny), n1+(nx*ny)+1, n1+(nx*ny)+nx+1, n1+(nx*ny)+nx];
 
-fprintf('\tMesh created successfully with %u Hex8 elements and %u nodes!\n\n',nelx*nely*nelz,nnod);
+fprintf('\n Mesh created successfully!\n');
+fprintf('\t Number of elements: %u\n',nelx*nely*nelz);
+fprintf('\t Number of nodes   : %u\n\n',nnod);
