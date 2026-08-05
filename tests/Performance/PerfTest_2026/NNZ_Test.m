546e47b4651e5a276aefd16074d8447fed05e013
fjramireg <fjramireg@gmail.com>
2026-08-05 12:13:20 -0500

    Updated tests

--- tests/Performance/PerfTest_2026/NNZ_Test.m
+++ tests/Performance/PerfTest_2026/NNZ_Test.m
@@ -1,13 +1,13 @@
-sets.nel = 160;
+sets.nel = 320;
 sets.dTE = 'uint32';
-sets.dTN = 'double';
+sets.dTN = 'single';
 [Mesh.elements, Mesh.nodes] = CreateMesh2(sets.nel,sets.nel,sets.nel,sets);
-sets.nel = 4096000;
-sets.nnodes = 4173281;
+sets.nel = 32768000;
+sets.nnodes = 33076161;
 sets.sz = 300;
 sets.edof = 24;
 MP.E = 200000000000;
 MP.nu = 3.000000e-01;
 
-%% NNZ_CPU_Vector_double_160
+%% NNZ_CPU_Vector_single_320
 Ke = eStiff_vssa(Mesh, MP, sets);
